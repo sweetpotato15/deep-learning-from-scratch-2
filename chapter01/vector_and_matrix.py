602ba5856cfa184fa54a06a8ee9ea8f5e3b6f429
sweetpotato15 <susan6439@naver.com>
2024-04-16 14:17:22 +0900

    벡터, 행렬 넘파이 연산

--- chapter01/vector_and_matrix.py
+++ chapter01/vector_and_matrix.py
@@ -1,3 +1,25 @@
 import numpy as np
 x = np.array([1,2,3])
 print(x.__class__)
+
+print(x.shape)
+print(x.ndim)
+
+W = np.array([[1,2,3],[4,5,6]])
+print(W.shape)
+print(W.ndim)
+
+W = np.array([[1,2,3],[4,5,6]])
+X = np.array([[0,1,2],[3,4,5]])
+print(W+X)
+print(W*X)
+
+# 벡터의 내적 
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(np.dot(a,b))
+
+# 행렬의 곱
+A = np.array([[1,2],[3,4]])
+B = np.array([[5,6],[7,8]])
+print(np.matmul(A,B))
